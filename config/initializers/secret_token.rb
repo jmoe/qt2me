4a793fd404c0023bb8feba525eae95dd12459b85
jerememonteau <me@jmoe.com>
2012-06-07 15:45:17 -0700

    quick commit 2012-06-07 15:45:17

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qt2me::Application.config.secret_token = '0f38db4c57c6ca4ffe7d284e22c46498cb48417b4dc54459ae73797bbf23b49bcee44ea24c9ceecf262df30e5752d1ce995da31288ee5dc04912250d306fe595'
