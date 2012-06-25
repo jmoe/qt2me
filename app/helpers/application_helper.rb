b5183043190e08f080eb5f733ee42e4b08e62d9d
Tom Collier <tcollier@gmail.com>
2012-06-25 16:20:51 -0700

    styling changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_price(price_in_cents)
+    if price_in_cents % 100 == 0
+      "$#{price_in_cents / 100}"
+    else
+      "$#{"%.2f" % (price_in_cents.to_f / 100.0)}"
+    end
+  end
 end
